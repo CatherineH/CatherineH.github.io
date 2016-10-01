5a6c011405bf7af1b1897f3709d9221997d3f0db
Catherine Holloway <milankie@gmail.com>
2016-10-01 09:34:43 -0700

    added bandcamp september post

---
+++ _posts/2016-09-30-stuff-i-bought-on-bandcamp-in-september.md
@@ -0,0 +1,45 @@
+---
+layout: post
+title: "Stuff I bought on Bandcamp in September"
+description: "but actually more like stuff from google play"
+category: music
+tags: [clipping, ghost, kansas]
+---
+{% include JB/setup %}
+
+The title of this post is a bit of a cheat; two of these albums actually came from Google Play. There are very few bands I'm willing to make a Google Play exception for.
+
+Splendor & Misery - Clipping.
+=============================
+
+[![Splendor & Misery - clipping.](https://f4.bcbits.com/img/a2530397806_14.jpg)](https://clppng.bandcamp.com/album/splendor-misery)
+
+Clipping's latest album is an afrofuturist rap concept album about an escaped slave adrift in space. It's great. The subject matter should make this an accessible album for fans of genres outside of rap where nerdy concept albums are common, such as musicals or prog rock. The instrumentals, lyrics and volume levels are all a bit toned down from Clipping's previous albums. Although concerned with very different subject matter, I've heard that there are references to their previous albums that have likely been documented on [rap genius](http://genius.com/albums/Clipping/Splendor-misery). I haven't looked through the annotations yet, as finding new references after multiple listens is part of Clipping's appeal and I don't want spoilers. 
+
+Popestar - Ghost
+================
+
+[![ Popestar - Ghost B.C.](https://lh3.googleusercontent.com/vUoEXye6GTgaiPeXLF1URizo-VtkStZtJDZzZdg02ti0BDCPrW0uMCm9BVJst4qE8WFiZ2hK=w300)](https://play.google.com/store/music/album/Ghost_B_C_Popestar?id=Bi52xos3n4petfxyal2j373nksu&hl=en)
+
+**Ghost** are still on their post-Meliora tour, though they are already planning their next album and will be in the studio once their tour ends in 2017. In the meantime they released an EP with one new song and four covers, and I was one of the 21,000 people who bought it. The new song is great and seems the suited, out of most of any of the Ghost discography, to Papa Emeritus III's vocal range. The covers are not great, though I am biased against covers. Ghost puts their own, different and talented version of all three songs, but become stale on second listen. The problem is the composition. A nameless ghoul called **The Echo and the Bunnyman** album *Ocean Drive* one of his favourite albums, so the *Nocturnal Me* cover is unsurprising. It's clear that The Echo and the Bunnyman, though in a different genre, influenced the richness and dourness of Ghost's sound. But unlike most Ghost songs, *Nocturnal Me* has no clear developement and ending. Two of the other covers, *I Believe* and *Bible* also suffer from lack of structure. *Missionary Man* doesn't suffer from lack of structure, but like all **Eurthymics** songs, it uses cheap pop song tricks. 
+
+I'll only be keeping *Square Hammer*, but I'm so eager for more Ghost material that I'll take what I can get.
+
+
+
+The Prelude Implicit - Kansas
+=============================
+
+[![ The Prelude Implicit  - Kansas](https://lh3.googleusercontent.com/ADDjE83N6jN4yabzmaPYlibm2merXP8Qj7obMWLV6wZ4-iEvs6fJTrwymY7tlPwogJ9culeO-bo=w300)](https://play.google.com/store/music/album/Kansas_The_Prelude_Implicit?id=Bulernqg77ddd7xewgv765vsjda)
+
+**Kansas** has a new album 14 years and after arguably the two most important members, Livgren and Walsh, left the band. It's okay. It's better than their last album because Billy Greer, whose voice is past its prime, is no longer singing lead vocals. His replacement, Ronnie Platt is a bit nasal and has trouble enunciating.
+
+Now that the band's strongest songwriters have retired, the lyrics are really boring. *Visibility Zero* is a political protest song, which is a different subject to execute in this era, but they do it adequately. *The Unsung Heroes* sounds like something Bon Jovi could have released, and *Refugee* is an overly simplistic consciousness-raising song. I don’t disagree with the message, but I think that a little more art could be employed in the delivery. 
+
+On other songs seem to be trying to update the band's sound. *Camouflage* uses a few baby diatonic rotations that **Dream Theater** pioneered. *Rhythm in the Spirit* uses the distorted but still poppy vocals that came out of nu-metal. However, both of these updates seem like too little, too late. If they're not going to push the envelope, I would have preferred a return to organs and violins.
+
+The two covers at the end are the worst things on the album. The *Home on the Range* cover is weird, and simultaneously too slow and too fast. The instrumental breaks are neat but out of place. The *Oh Shenandoah* cover is a bit more listenable because there are no lyrics, but it’s still boring - it sounds like a song that might play on an Aaron Sworkin movie. 
+
+Despite all of its flaws, the solos are as good as ever. If you're a Kansas fan, it's worth a listen, but I wouldn't recommend it as an entry point to their discography.
+
+
--- _scripts/completion.py
+++ _scripts/completion.py
@@ -1,19 +1,49 @@
 """
 The following script contains several helpers to complete images and links.
 """
-from os import environ
-from os.path import expanduser
+from HTMLParser import HTMLParser
+from os import environ, mkdir, listdir
+from os.path import expanduser, isdir, basename, join, exists, dirname, \
+    realpath
 from sys import argv
+from time import sleep
 
 from bandcamp_dl import Bandcamp
 
 from apiclient.discovery import build
-
+from wget import download
 
 DEVELOPER_KEY = environ['GOOGLE_API_KEY']
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
+TEMP_DIR = expanduser("~/tmp")
+CURRENT_DIRECTORY = dirname(realpath(__file__))
 
+
+class GooglePlayParser(HTMLParser):
+    grab_data = False
+    def handle_starttag(self, tag, attrs):
+        # grab the link to the album art
+        if tag == "img":
+            grab_src = False
+            for attr in attrs:
+                if attr[0] == "alt":
+                    if attr[1].find("Cover art") >= 0:
+                        grab_src = True
+            if grab_src:
+                for attr in attrs:
+                    if attr[0] == "src":
+                        self.found_url = attr[1]
+        # grab the page title to get the title and album artist
+        if tag == "title":
+            self.grab_data = True
+        else:
+            self.grab_data = False
+
+    def handle_data(self, data):
+        if self.grab_data:
+            self.artist = data.split(":")[0]
+            self.title = data.split(":")[1].replace(" - Music on Google Play", "")
 
 def get_title(id):
     youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
@@ -27,56 +57,86 @@ def get_title(id):
     return video_response["snippet"]["title"]
 
 
-def youtube(input_file):
-    """
-    Replace the youtube urls with screenshots and links
-    :param input_file: the file to analyze
-    :return:
-    """
+def substitute_file(input_file, replacer_function):
     fh = open(input_file, "r")
     _text = fh.read(-1)
     fh.close()
     # split on the spaces
     _words = _text.split("\n")
     for i in range(len(_words)):
-        https_part = "https://www.youtube.com/watch?v="
-        if _words[i].find(https_part) == 0:
-            token = _words[i].replace(https_part, '')
-            desc = get_title(token)
-            _words[i] = "[!["+desc+"](http://img.youtube.com/vi/"+token + \
-                       "/0.jpg)]("+_words[i]+")"
-    output_file = input_file+"-copy"
+        _words[i] = replacer_function(_words[i])
+    output_file = join(TEMP_DIR, basename(input_file + "-copy"))
     fh = open(output_file, "w+")
     fh.write('\n'.join(_words))
     fh.close()
     return output_file
+
+
+def youtube(input_file):
+    """
+    Replace the youtube urls with screenshots and links
+
+    :param input_file: the file to analyze
+    :return:
+    """
+    def replacer(word):
+        https_part = "https://www.youtube.com/watch?v="
+        if word.find(https_part) == 0:
+            token = word.replace(https_part, '')
+            desc = get_title(token)
+            return "[![" + desc + "](http://img.youtube.com/vi/" + token + \
+                        "/0.jpg)](" + word + ")"
+        return word
+    return substitute_file(input_file, replacer)
 
 
 def bandcamp(input_file):
     """
     Replace bandcamp urls with album covers and links back to the album.
-    :param input_file: the file to analyze
-    :return:
-    """
-    fh = open(input_file, "r")
-    _text = fh.read(-1)
-    fh.close()
-    _words = _text.split("\n")
-    for i in range(len(_words)):
-        if _words[i].find("bandcamp.com/album") >= 0:
-            url = _words[i]
-            parser = Bandcamp().parse(url)
-            album_art = parser["art"].replace("_16.jpg", "_14.jpg")
-            _words[i] = "[!["+parser['title']+" - "+parser["artist"]+"]("+\
-                        album_art+")]("+url+")"
-    output_file = input_file+"-copy"
-    fh = open(output_file, "w+")
-    fh.write('\n'.join(_words))
-    fh.close()
-    return output_file
 
+    :param str input_file: the file to analyze
+    :return: the filename of the output file
+    :rtype: str
+    """
+    def replacer(word):
+        if word.find("bandcamp.com/album") >= 0:
+            parser = Bandcamp().parse(word)
+            album_art = parser["art"].replace("_16.jpg", "_14.jpg")
+            return "[!["+parser['title']+" - "+parser["artist"]+"]("+\
+                        album_art+")]("+word+")"
+        return word
+    return substitute_file(input_file, replacer)
+
+
+def google_play(input_file):
+    """
+    Replace google play urls with album covers and links back to the album
+
+    :param input_file: the file to analyze
+    :return: the filename of the output file
+    :rtype: str
+    """
+    def replacer(word):
+        if word.find("play.google.com") >= 0:
+            target_filename = join(TEMP_DIR, basename(word))
+            if not exists(target_filename):
+                filename = download(word, target_filename)
+                sleep(1)
+                #rename(join(CURRENT_DIRECTORY, filename), target_filename)
+            parser = GooglePlayParser()
+            contents = open(target_filename, "r").read(-1)
+            parser.feed(contents)
+            album_art = parser.found_url
+            return "[![" + parser.title + " - " + parser.artist + "](" + \
+                   album_art + ")](" + word + ")"
+        return word
+    if not isdir(TEMP_DIR):
+        mkdir(TEMP_DIR)
+    return substitute_file(input_file, replacer)
 
 if __name__ == "__main__":
     input_file = expanduser(argv[1])
-    output_file = youtube(input_file=input_file)
-    bandcamp(output_file)
+    output_file = input_file
+    #output_file = youtube(input_file=output_file)
+    output_file = bandcamp(output_file)
+    output_file = google_play(output_file)
