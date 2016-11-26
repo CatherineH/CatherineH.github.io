bf582f8fcdedbbb5dda333ab11e424c92f2f377b
Catherine Holloway <milankie@gmail.com>
2016-11-26 13:36:37 -0800

    published opening acts post

---
+++ _posts/2016-11-26-opening-acts-1.md
@@ -0,0 +1,44 @@
+---
+layout: post
+title: "Opening Acts 1"
+description: ""
+category: Music
+tags: [Fallujah, Pallbearer, Helen Money]
+---
+{% include JB/setup %}
+
+This post covers some albums I purchased by bands I encountered as opening acts at concerts I've recently attended.
+
+
+Fallujah
+========
+
+<table><tr><td><a href="https://uniqueleaderrecords.bandcamp.com/album/the-flesh-prevails"><img src="https://f4.bcbits.com/img/a1078697972_14.jpg" alt="The Flesh Prevails - Fallujah"></a></td>
+<td><a href="https://uniqueleaderrecords.bandcamp.com/album/the-harvest-wombs"><img src="https://f4.bcbits.com/img/a1464302985_14.jpg" alt="The Harvest Wombs - Fallujah"></a></td>
+<td><a href="https://fallujah.bandcamp.com/album/nomadic"><img src="https://f4.bcbits.com/img/a0214343064_14.jpg" alt="-NOMADIC- - Fallujah"></a></td></tr></table>
+
+San Francisco's **Fallujah** opened for **Between the Buried and Me**. *Fallujah* is progressive djenty metal crossed with **Aphex-Twin**, which is refreshing mix. Their style oscillates between being an emo **Fleshgod Apocalypse** or what **Jean Michel Jarre**'s metalcore experiments might have sounded like.
+
+So far I like this band a lot, but I have two complaints. The changes between technical death metal and melodic parts are very abrupt, especially on *Harvest Womb*'s *Enslaved Eternal Phenomenon*. Their ambiance verges on filler - such as *Nomadic EP's* *Silent*. I'm not sure if I can think of a situation appropriate for some of their songs - it's too mellow to be programming or exercising music, but too metal for an intimate party or reading in bed. Similarly, *Venom on the Blade* is another track that has a great first two minutes but becomes annoyingly lethargic by minute 5.
+
+Fallujah is not the most progressive band, but they encorporate new sounds and their execution is competent. If you want some fresh-sounding technical, check them out.
+
+
+Pallbearer
+==========
+
+<table><tr><td><a href="https://play.google.com/store/music/album/Pallbearer_Sorrow_And_Extinction?id=Brtj6qg7ioooensrjlwhp4nh3o4"><img src="https://lh6.googleusercontent.com/u321h-hPfyJ-hTVKhe3h6tAghWm4TBZ-M4kys1n_siJ8Y5-fP0fPxxJoRZcWGw2K75wFzKX2Mw=w300" alt=" Sorrow And Extinction - Pallbearer"></a></td>
+<td><a href="https://play.google.com/store/music/album/Pallbearer_Foundations_Of_Burden?id=Bpvmhgrforzqux3bkgpg43cm2tq"><img src="https://lh3.googleusercontent.com/25V6ASP0nl4N1Q5ObI7jungRaiSQ8TEl05shlzuLQqd6fwhWsVyflcZEj0GWnnMy04OBan-V=w300" alt=" Foundations Of Burden - Pallbearer"></a></td>
+<td><a href="https://play.google.com/store/music/album/Pallbearer_Fear_Fury_EP?id=Bbjoxzywp5aipfhqenbd2qo262e"><img src="https://lh3.googleusercontent.com/8DifgtK0zynVxemH771yvt6iPghvv3APxArx_wlRfspQuOb0ViQQejH2w6r2MQJmBksgM_qG=w300" alt=" Fear  Fury EP - Pallbearer"></a></td></tr></table>
+
+**Pallbearer** opened for **Baroness**. Though classified as doom metal, the slowness of their playing reminds me more of stoner metal bands like **Sleep**, though with **Baroness**'s chords. They're good, but have neither the melodic or rhythmic complexity of **Sleep** nor the emotional breadth or depth of **Baroness*. The singing is overly mournful, such as on their cover of **Black Sabbath**'s *Over and Over*, or songs take way too long to get started, such as on *Sorrow and Extinction*'s *Foreigner*.
+
+Still, tracks that don't have much singing are great. If you like **Baroness**, check them out.
+
+Helen Money
+===========
+
+<table><tr><td><a href="https://helenmoney.bandcamp.com/album/become-zero"><img src="https://f4.bcbits.com/img/a2485387565_14.jpg" alt="Become Zero - Helen Money"></a></td>
+<td><a href="https://helenmoney.bandcamp.com/album/in-tune"><img src="https://f4.bcbits.com/img/a0131647649_14.jpg" alt="In Tune - Helen Money"></a></td></tr></table>
+
+**Helen Money** is a cellist and composer who opened for **MAGMA**. Unlike the previous two bands, she shares nothing with the act she opened for, except for an appreciation of orchestral instruments. I would classify her as being in the category of metal-adjacent experimental musicians like **Chelsea Wolfe**. Her music would be the perfect soundtrack to a purposefully tonally jarring Sci-Fi movie like *Under the Skin* or *Melancholia*. Her music may not be something to mosh to, but it is great music for writing down your dreams to.
--- _scripts/completion.py
+++ _scripts/completion.py
@@ -22,6 +22,10 @@ CURRENT_DIRECTORY = dirname(realpath(__file__))
 
 class GooglePlayParser(HTMLParser):
     grab_data = False
+    title_data = ""
+    artist = None
+    title = None
+
     def handle_starttag(self, tag, attrs):
         # grab the link to the album art
         if tag == "img":
@@ -39,11 +43,20 @@ class GooglePlayParser(HTMLParser):
             self.grab_data = True
         else:
             self.grab_data = False
+            if len(self.title_data) > 0:
+                if self.title_data.find(":") > 0:
+                    self.artist = self.title_data.split(":")[0]
+                    self.title = self.title_data.split(":")[1].replace(
+                        " - Music on Google Play", "")
+                else:
+                    raise Exception(
+                        "could not find deliminator in title':' in ",
+                        self.title_data)
 
     def handle_data(self, data):
         if self.grab_data:
-            self.artist = data.split(":")[0]
-            self.title = data.split(":")[1].replace(" - Music on Google Play", "")
+            self.title_data += data
+
 
 def get_title(id):
     youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
@@ -51,23 +64,27 @@ def get_title(id):
 
     # Call the videos.list method to retrieve location details for each video.
     video_response = youtube.videos().list(
-    id=id,
-    part='snippet'
+        id=id,
+        part='snippet'
     ).execute().get("items", [])[0]
     return video_response["snippet"]["title"]
 
 
-def substitute_file(input_file, replacer_function):
+def substitute_file(input_file, replacer_function, on_lines=True):
     fh = open(input_file, "r")
     _text = fh.read(-1)
     fh.close()
     # split on the spaces
-    _words = _text.split("\n")
-    for i in range(len(_words)):
-        _words[i] = replacer_function(_words[i])
+    if on_lines:
+        _words = _text.split("\n")
+        for i in range(len(_words)):
+            _words[i] = replacer_function(_words[i])
+        output_contents = '\n'.join(_words)
+    else:
+        output_contents = replacer_function(_text)
     output_file = join(TEMP_DIR, basename(input_file + "-copy"))
     fh = open(output_file, "w+")
-    fh.write('\n'.join(_words))
+    fh.write(output_contents)
     fh.close()
     return output_file
 
@@ -79,14 +96,17 @@ def youtube(input_file):
     :param input_file: the file to analyze
     :return:
     """
+
     def replacer(word):
         https_part = "https://www.youtube.com/watch?v="
         if word.find(https_part) == 0:
             token = word.replace(https_part, '')
             desc = get_title(token)
-            return "[![" + desc + "](http://img.youtube.com/vi/" + token + \
-                        "/0.jpg)](" + word + ")"
+            return "<a href=\"" + word + "\">" \
+                   "<img src=\"http://img.youtube.com/vi/" + token + \
+                   "/0.jpg\" alt=\"" + desc + "\"></a>"
         return word
+
     return substitute_file(input_file, replacer)
 
 
@@ -98,13 +118,15 @@ def bandcamp(input_file):
     :return: the filename of the output file
     :rtype: str
     """
+
     def replacer(word):
         if word.find("bandcamp.com/album") >= 0:
             parser = Bandcamp().parse(word)
             album_art = parser["art"].replace("_16.jpg", "_14.jpg")
-            return "[!["+parser['title']+" - "+parser["artist"]+"]("+\
-                        album_art+")]("+word+")"
+            return "<a href=\""+word+"\"><img src=\""+album_art+"\" alt=\"" + parser['title'] + \
+                   " - " + parser["artist"] + "\"></a>"
         return word
+
     return substitute_file(input_file, replacer)
 
 
@@ -116,27 +138,65 @@ def google_play(input_file):
     :return: the filename of the output file
     :rtype: str
     """
+
     def replacer(word):
         if word.find("play.google.com") >= 0:
             target_filename = join(TEMP_DIR, basename(word))
             if not exists(target_filename):
                 filename = download(word, target_filename)
                 sleep(1)
-                #rename(join(CURRENT_DIRECTORY, filename), target_filename)
+                # rename(join(CURRENT_DIRECTORY, filename), target_filename)
             parser = GooglePlayParser()
             contents = open(target_filename, "r").read(-1)
-            parser.feed(contents)
+            try:
+                parser.feed(contents)
+            except Exception as e:
+                print("got exception: ", e, " on ", target_filename)
             album_art = parser.found_url
-            return "[![" + parser.title + " - " + parser.artist + "](" + \
-                   album_art + ")](" + word + ")"
+            return "<a href=\"" + word + "\"><img src=\"" + album_art + "\" alt=\"" + \
+                   parser.title + " - " + parser.artist + "\"></a>"
         return word
+
     if not isdir(TEMP_DIR):
         mkdir(TEMP_DIR)
     return substitute_file(input_file, replacer)
 
+
+def tablelize(input_file):
+    """
+    combine images into a table
+
+    :param input_file: the contents of the file
+    :return:
+    """
+
+    def replacer(contents):
+        _lines = contents.split('\n')
+        image_count = 0
+        for replacer_i in range(len(_lines)):
+            if _lines[replacer_i].find("<a ") == 0:
+                image_count = image_count+1
+            else:
+                if image_count > 0:
+                    for j in range(1, image_count + 1):
+                        _lines[replacer_i - j] = "<td>" + _lines[replacer_i - j] + "</td>"
+                        if image_count == j:
+                            _lines[replacer_i - j] = "<table><tr>" + _lines[replacer_i - j]
+                        elif j == 1:
+                            _lines[replacer_i - j] = _lines[replacer_i - j] + "</tr></table>"
+                image_count = 0
+        out_contents = "\n".join(_lines)
+        return out_contents
+
+    if not isdir(TEMP_DIR):
+        mkdir(TEMP_DIR)
+    return substitute_file(input_file, replacer, on_lines=False)
+
+
 if __name__ == "__main__":
     input_file = expanduser(argv[1])
     output_file = input_file
-    #output_file = youtube(input_file=output_file)
+    # output_file = youtube(input_file=output_file)
     output_file = bandcamp(output_file)
     output_file = google_play(output_file)
+    output_file = tablelize(output_file)
